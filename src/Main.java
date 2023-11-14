394a4e9fd75ba9f7ead876afc3791b644353e1e8
Kerem Karaman <keremkaraman91@gmail.com>
2023-11-14 12:41:50 +0300

    ilk görev yapıldı

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(isPalindrome(-121));
+    }
+
+    public static boolean isPalindrome(int num) {
+        int posNum = Math.abs(num);
+        String strNum = String.valueOf(posNum);
+        String reversedStr = new StringBuilder(strNum).reverse().toString();
+        int reversedNum = Integer.parseInt(reversedStr);
+        return posNum == reversedNum;
+    }
+
+    public static boolean isPerfectNumber(int num) {
+
+
     }
 }
